569522647b2b2f1f9bd4c984dbe2150fb1a3d546
Hieu Pham <hieupm2096@gmail.com>
2021-09-30 16:02:20 +0700

    add: cors, helmet

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet'
 
 // initialize configuration
 dotenv.config()
 
 // get env
-const port = process.env.PORT;
+if (!process.env.PORT) {
+    process.exit(1)
+}
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
 const app = express();
 
-// define a route handler for default homepage
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // start the Express server
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`)
 });
